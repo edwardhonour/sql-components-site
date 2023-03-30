fe943ae11f107a44b9a0422b414abaaabe81cf0c
Ed Honour <40154656+edwardhonour@users.noreply.github.com>
2023-03-30 12:44:50 -0500

    12:44

--- src/app/admin/forms/facility-saa-type-form/facility-saa-type-form.component.ts
+++ src/app/admin/forms/facility-saa-type-form/facility-saa-type-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SqlComponentsModule, SqlDataSelectComponent, SqlSubmitComponent } from 'sql-components';
+import { SqlComponentsModule, SqlDataSelectComponent, SqlDeleteComponent, SqlSubmitComponent } from 'sql-components';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-facility-saa-type-form',
   standalone: true,
-  imports: [CommonModule, SqlComponentsModule, SqlSubmitComponent, SqlDataSelectComponent],
+  imports: [CommonModule, SqlComponentsModule, SqlSubmitComponent, SqlDataSelectComponent, SqlDeleteComponent, MatDialogModule],
   templateUrl: './facility-saa-type-form.component.html',
   styleUrls: ['./facility-saa-type-form.component.css']
 })
